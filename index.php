dee99758a346e82932943f9fb510f20ea3b66aa2
Luiza Avelino <avelinoluizaalmeida@gmail.com>
2019-04-22 00:05:25 -0300

    adicionei index.php

--- index.php
+++ index.php
@@ -1,30 +1,4 @@
 <?php
-    #camelCase
-    $anoNascimento = 1998;
-    $nomeCompleto = "";
-    $nome1 = 'Luiza';
-    $sobrenome = 'Avelino';
-    $nomeCompleto = $nome1 ." ". $sobrenome;
-    echo $nomeCompleto;
-    exit;
-    //Comentário
-    /*
-        Multiplo comentario
-    */
-
-    echo $nome1;
-
-    #"Apagar" a váriavel da memória
-    #unset($nome1);
-
-    echo "<br>";
-    echo $nome1;
-
-    #verificar se está definido
-
-    if (isset($nome1)) {
-        echo $nome1;
-    }
-
+    echo "teste";
 
 ?>
